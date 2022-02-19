73212ca36039fddc21b0a6c92022775fc64deb3a
Frank Burnham <frank@n8git.dev>
2022-02-19 15:59:52 -0500

    Isolate store and process

--- data.rkt
+++ data.rkt
@@ -2,6 +2,7 @@
 
 (provide start
          store
+         process
          data)
 
 (require "data/types.rkt"
@@ -10,11 +11,15 @@
 (: data* (Listof request-response))
 (define data* '())
 
-(: store (-> middleware:middlewares request-response Void))
-(define (store mids req-resp)
+(: store (-> request-response Void))
+(define (store req-resp)
   ;; run some normalizing stuff like decoding the gzipped data
   ;; and storing it as bytes
-  (set! data* (cons (middleware:apply mids req-resp) data*)))
+  (set! data* (cons req-resp data*)))
+
+(: process (-> middleware:middlewares request-response request-response))
+(define (process mids req-resp)
+  (middleware:apply mids req-resp))
 
 (: data (-> (Listof request-response)))
 (define (data) ; dunno if I care to keep this... it does keep data* safe from set! happening elsewhere
